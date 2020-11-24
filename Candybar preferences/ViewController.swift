ff50aadc629964a38a4cb5a629a8048499276a74
Gianpiero <gianpispi@me.com>
2020-11-24 12:07:17 +0100

    Initial Commit

---
+++ Candybar preferences/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Candybar preferences
+//
+//  Created by Gianpiero Spinelli on 24/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
